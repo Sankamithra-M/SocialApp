ad099a7ac6d5aada2ad08a219769e804a336c16e
Sankamithra M <sankamithra1614@gmail.com>
2026-08-05 15:29:43 +0530

    follower and following list, follow status , transaction

--- backend/src/modules/follows/follow.service.js
+++ backend/src/modules/follows/follow.service.js
@@ -1,6 +1,7 @@
 import Follow from "./follow.model.js";
 import User from "../users/user.model.js";
 import AppError from "../../errors/AppError.js";
+import mongoose from "mongoose";
 
 
 export const followUserService = async (
@@ -8,60 +9,92 @@ export const followUserService = async (
   username
 ) => {
 
-const targetUser = await User.findOne({
-  username,
-});
+  const session = await mongoose.startSession();
 
-if (!targetUser) {
-  throw new AppError("User not found", 404);
-}
+  try {
 
-if (currentUserId.equals(targetUser._id)) {
-  throw new AppError(
-    "You cannot follow yourself",
-    400
-  );
-}
+    session.startTransaction();
 
-const existingFollow = await Follow.findOne({
-  follower: currentUserId,
-  following: targetUser._id,
-});
+    const targetUser = await User.findOne({
+      username,
+    }).session(session);
 
-if (existingFollow) {
-  throw new AppError(
-    "You are already following this user",
-    400
-  );
-}
+    if (!targetUser) {
+      throw new AppError("User not found", 404);
+    }
 
-await Follow.create({
-  follower: currentUserId,
-  following: targetUser._id,
-});
+    if (currentUserId.equals(targetUser._id)) {
+      throw new AppError(
+        "You cannot follow yourself",
+        400
+      );
+    }
 
+    const existingFollow = await Follow.findOne({
+      follower: currentUserId,
+      following: targetUser._id,
+    }).session(session);
 
-await User.findByIdAndUpdate(
-  currentUserId,
-  {
-    $inc: {
-      followingCount: 1,
-    },
+    if (existingFollow) {
+      throw new AppError(
+        "Already following this user",
+        400
+      );
+    }
+
+    await Follow.create(
+      [
+        {
+          follower: currentUserId,
+          following: targetUser._id,
+        },
+      ],
+      {
+        session,
+      }
+    );
+
+    await User.findByIdAndUpdate(
+      currentUserId,
+      {
+        $inc: {
+          followingCount: 1,
+        },
+      },
+      {
+        session,
+      }
+    );
+
+    await User.findByIdAndUpdate(
+      targetUser._id,
+      {
+        $inc: {
+          followersCount: 1,
+        },
+      },
+      {
+        session,
+      }
+    );
+
+    await session.commitTransaction();
+
+    return {
+      message: "User followed successfully",
+    };
+
+  } catch (error) {
+
+    await session.abortTransaction();
+
+    throw error;
+
+  } finally {
+
+    session.endSession();
+
   }
-);
-
-await User.findByIdAndUpdate(
-  targetUser._id,
-  {
-    $inc: {
-      followersCount: 1,
-    },
-  }
-);
-return {
-  message: "User followed successfully",
-};
-
 };
 
 
@@ -70,7 +103,141 @@ export const unfollowUserService = async (
   currentUserId,
   username
 ) => {
-  // Find target user
+  const session = await mongoose.startSession();
+
+  try {
+    session.startTransaction();
+
+    // Find target user
+    const targetUser = await User.findOne({
+      username,
+    }).session(session);
+
+    if (!targetUser) {
+      throw new AppError("User not found", 404);
+    }
+
+    // Prevent self unfollow
+    if (currentUserId.equals(targetUser._id)) {
+      throw new AppError(
+        "You cannot unfollow yourself",
+        400
+      );
+    }
+
+    // Find and delete follow relationship
+    const deletedFollow =
+      await Follow.findOneAndDelete(
+        {
+          follower: currentUserId,
+          following: targetUser._id,
+        },
+        {
+          session,
+        }
+      );
+
+    if (!deletedFollow) {
+      throw new AppError(
+        "You are not following this user",
+        400
+      );
+    }
+
+    // Decrease following count
+    await User.findByIdAndUpdate(
+      currentUserId,
+      {
+        $inc: {
+          followingCount: -1,
+        },
+      },
+      {
+        session,
+      }
+    );
+
+    // Decrease followers count
+    await User.findByIdAndUpdate(
+      targetUser._id,
+      {
+        $inc: {
+          followersCount: -1,
+        },
+      },
+      {
+        session,
+      }
+    );
+
+    await session.commitTransaction();
+
+    return {
+      message: "User unfollowed successfully",
+    };
+
+  } catch (error) {
+
+    await session.abortTransaction();
+
+    throw error;
+
+  } finally {
+
+    session.endSession();
+
+  }
+};
+export const getFollowersService = async (
+  username
+) => {
+  const user = await User.findOne({
+    username,
+  });
+
+  if (!user) {
+    throw new AppError("User not found", 404);
+  }
+
+  const followers = await Follow.find({
+    following: user._id,
+  }).populate(
+    "follower",
+    "username displayName profileImage"
+  );
+
+  return followers.map(
+    (follow) => follow.follower
+  );
+};
+
+export const getFollowingService = async (
+  username
+) => {
+  const user = await User.findOne({
+    username,
+  });
+
+  if (!user) {
+    throw new AppError("User not found", 404);
+  }
+
+  const following = await Follow.find({
+    follower: user._id,
+  }).populate(
+    "following",
+    "username displayName profileImage"
+  );
+
+  return following.map(
+    (follow) => follow.following
+  );
+};
+
+export const checkFollowStatusService = async (
+  currentUserId,
+  username
+) => {
   const targetUser = await User.findOne({
     username,
   });
@@ -79,45 +246,12 @@ export const unfollowUserService = async (
     throw new AppError("User not found", 404);
   }
 
-  // Prevent self unfollow
-  if (currentUserId.equals(targetUser._id)) {
-    throw new AppError(
-      "You cannot unfollow yourself",
-      400
-    );
-  }
-
-  // Find and delete follow relationship
-  const deletedFollow =
-    await Follow.findOneAndDelete({
-      follower: currentUserId,
-      following: targetUser._id,
-    });
-
-  // User was not following
-  if (!deletedFollow) {
-    throw new AppError(
-      "You are not following this user",
-      400
-    );
-  }
-
-  // Decrease following count of current user
-  await User.findByIdAndUpdate(currentUserId, {
-    $inc: {
-      followingCount: -1,
-    },
-  });
-
-  // Decrease followers count of target user
-  await User.findByIdAndUpdate(targetUser._id, {
-    $inc: {
-      followersCount: -1,
-    },
+  const follow = await Follow.findOne({
+    follower: currentUserId,
+    following: targetUser._id,
   });
 
   return {
-    message: "User unfollowed successfully",
+    isFollowing: !!follow,
   };
 };
-
